38665174ebd13bbc833548a9545bf5b77982a852
Utsav Joshi <joshiutsav56@gmail.com>
2023-08-04 20:55:26 +0530

    Added Search Button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
-import './App.css';
-import logo from './mlh-prep.png';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import logo from "./mlh-prep.png";
 
 function App() {
   const [error, setError] = useState(null);
@@ -25,56 +25,77 @@ function App() {
     }
   }, []);
 
-  
   const fetchWeatherByCoordinates = (latitude, longitude) => {
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${process.env.REACT_APP_APIKEY}`;
+    fetchWeatherData(url);
+  };
+
+  const fetchWeatherByCity = () => {
+    if (city.trim() !== "") {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_APIKEY}`;
+      fetchWeatherData(url);
+    } else {
+      setError("Please enter a valid city name.");
+    }
+  };
+
+  const fetchWeatherData = (url) => {
     fetch(url)
-    .then(res => res.json())
-    .then(
-      (result) => {
-        if (result.cod !== 200) {
-          setIsLoaded(false);
-        } else {
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          if (result.cod !== 200) {
+            setIsLoaded(false);
+            setError("City not found");
+          } else {
+            setIsLoaded(true);
+            setResults(result);
+            setCity(result.name + ", " + result.sys.country);
+          }
+        },
+        (error) => {
           setIsLoaded(true);
-          setResults(result);
-          setCity(result.name + ", " + result.sys.country);
+          setError("Error fetching weather data.");
         }
-      },
-      (error) => {
-        setIsLoaded(true);
-        setError(error);
-      }
       );
   };
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  } else {
-    return (
-      <>
-        <img className="logo" src={logo} alt="MLH Prep Logo" />
-        <div>
-          <h2>Enter a city below 👇</h2>
-          <input
-            type="text"
-            placeholder="Search..."
-            value={city}
-            onChange={event => setCity(event.target.value)}
-          />
-          <div className="Results">
-            {!isLoaded && <h2>Loading...</h2>}
-            {isLoaded && results && (
-              <>
-                <h3>{results.weather[0].main}</h3>
-                <p>Feels like {results.main.feels_like}°C</p>
-                <i><p>{results.name}, {results.sys.country}</p></i>
-              </>
-            )}
-          </div>
+  return (
+    <>
+      <img className="logo" src={logo} alt="MLH Prep Logo" />
+      <div>
+        <h2>Enter a city below 👇</h2>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={city}
+          onChange={(event) => setCity(event.target.value)}
+        />
+        <button
+          className="search"
+          onClick={fetchWeatherByCity}
+          disabled={!city.trim()} // Disable the button when there is no input
+        >
+          Search
+        </button>
+        <div className="Results">
+          {!isLoaded && <h2>Loading...</h2>}
+          {isLoaded && results && (
+            <>
+              <h3>{results.weather[0].main}</h3>
+              <p>Feels like {results.main.feels_like}°C</p>
+              <i>
+                <p>
+                  {results.name}, {results.sys.country}
+                </p>
+              </i>
+            </>
+          )}
+          {error && <div>Error: {error}</div>}
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 }
 
 export default App;
